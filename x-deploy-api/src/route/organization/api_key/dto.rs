c718440e5c583502d30e2939461dd7ed6f9b6f8b
Florian <91879250+florianepitech@users.noreply.github.com>
2024-01-24 00:04:44 -0500

    add: exemple for api key dto

--- x-deploy-api/src/route/organization/api_key/dto.rs
+++ x-deploy-api/src/route/organization/api_key/dto.rs
@@ -1,7 +1,14 @@
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use utoipa::ToSchema;
 
 #[derive(Deserialize, Serialize, Debug, ToSchema)]
+#[schema(example = json!({
+    "name": "My Stunning Organization",
+    "description": "A new amazing organization !",
+    "role_id": "5f7b1b1b1b1b1b1b1b1b1b1b",
+    "expires_at": "2021-01-01T00:00:00Z",
+}))]
 pub struct CreateApiKeyRequest {
   #[serde(rename = "name")]
   pub name: String,
@@ -19,6 +26,10 @@ pub struct CreateApiKeyRequest {
 }
 
 #[derive(Deserialize, Serialize, Debug, ToSchema)]
+#[schema(example = json!({
+    "id": "5f7b1b1b1b1b1b1b1b1b1b1b",
+    "key": "hhqtOgA7boOx9hK...",
+}))]
 pub struct CreateApiKeyResponse {
   #[serde(rename = "id")]
   pub id: String,
@@ -28,6 +39,19 @@ pub struct CreateApiKeyResponse {
 }
 
 #[derive(Deserialize, Serialize, Debug, ToSchema)]
+#[schema(example = json!({
+    "id": "5f7b1b1b1b1b1b1b1b1b1b1b",
+    "organizationId": "5f7b1b1b1b1b1b1b1b1b1b1b",
+    "name": "My Stunning API Key",
+    "description": "A new amazing API Key !",
+    "role": {
+        "id": "5f7b1b1b1b1b1b1b1b1b1b1b",
+        "name": "Admin",
+        "description": "Admin role"
+    },
+    "createdAt": "2020-10-05T00:00:00Z",
+    "expiresAt": "2021-01-01T00:00:00Z",
+}))]
 pub struct ApiKeyInfoResponse {
   #[serde(rename = "id")]
   pub id: String,
@@ -52,6 +76,11 @@ pub struct ApiKeyInfoResponse {
 }
 
 #[derive(Deserialize, Serialize, Debug, ToSchema)]
+#[schema(example = json!({
+    "id": "5f7b1b1b1b1b1b1b1b1b1b1b",
+    "name": "Admin",
+    "description": "Admin role",
+}))]
 pub struct ApiKeyRoleInfoResponse {
   #[serde(rename = "id")]
   pub id: String,
@@ -64,6 +93,10 @@ pub struct ApiKeyRoleInfoResponse {
 }
 
 #[derive(Deserialize, Serialize, Debug, ToSchema)]
+#[schema(example = json!({
+    "id": "5f7b1b1b1b1b1b1b1b1b1b1b",
+    "password": "MyAmazingPassword123!",
+}))]
 pub struct DeleteApiKeyRequest {
   #[serde(rename = "id")]
   pub id: String,
@@ -73,6 +106,11 @@ pub struct DeleteApiKeyRequest {
 }
 
 #[derive(Deserialize, Serialize, Debug, ToSchema)]
+#[schema(example = json!({
+    "id": "5f7b1b1b1b1b1b1b1b1b1b1b",
+    "name": "New Role Admin Name",
+    "description": "New Role Admin Description",
+}))]
 pub struct UpdateApiKeyRequest {
   #[serde(rename = "name")]
   pub name: String,
